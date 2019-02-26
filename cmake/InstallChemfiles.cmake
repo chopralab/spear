ad51a644b97da8daa4704480d3e8d96627f4c8e5
Jonathan Fine <finej@purdue.edu>
2019-02-25 22:13:59 -0500

    Added ability to use external chemfiles

--- cmake/InstallChemfiles.cmake
+++ cmake/InstallChemfiles.cmake
@@ -3,20 +3,19 @@ include(ExternalProject)
 ExternalProject_Add( CHEMFILES
     GIT_REPOSITORY https://github.com/chemfiles/chemfiles.git
     SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/chemfiles_build
-    CMAKE_CACHE_ARGS    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/chemfiles
-                        -DBUILD_SHARED_LIBS:BOOL=ON
-                        -DCMAKE_BUILD_TYPE:STRING=Release
+    CMAKE_CACHE_ARGS
+        -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}/chemfiles
+        -DBUILD_SHARED_LIBS:BOOL=ON
+        -DCMAKE_BUILD_TYPE:STRING=Release
 )
 
 add_library(chemfiles SHARED IMPORTED)
-set_property(TARGET chemfiles PROPERTY IMPORTED_LOCATION
-    ${CMAKE_CURRENT_BINARY_DIR}/chemfiles/lib/${CMAKE_SHARED_LIBRARY_PREFIX}chemfiles${CMAKE_SHARED_LIBRARY_SUFFIX}
-)
-set_property(TARGET chemfiles PROPERTY IMPORTED_IMPLIB
-    ${CMAKE_CURRENT_BINARY_DIR}/chemfiles/lib/chemfiles.lib
+set_target_properties(
+    chemfiles PROPERTIES
+    IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/chemfiles/lib/${CMAKE_SHARED_LIBRARY_PREFIX}chemfiles${CMAKE_SHARED_LIBRARY_SUFFIX}
+    IMPORTED_IMPLIB ${CMAKE_CURRENT_BINARY_DIR}/chemfiles/lib/chemfiles.lib
 )
 
 add_dependencies(chemfiles CHEMFILES)
+include_directories(${CMAKE_CURRENT_BINARY_DIR}/chemfiles/include)
 
-set(CHEMFILES_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/chemfiles/include)
-set(CHEMFILES_LIBRARY chemfiles)
