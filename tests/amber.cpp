9650c4c0d1184134a4b6b3aab989128cdcded272
Jonathan Fine <finej@purdue.edu>
2019-05-18 21:43:25 -0400

    Add additional AMBER test

--- tests/amber.cpp
+++ tests/amber.cpp
@@ -59,3 +59,52 @@ TEST_CASE("Read old AMBER file") {
         }
     }
 }
+
+TEST_CASE("Read new AMBER file") {
+    std::ifstream amber_dat("share/ff10.xml");
+    std::ifstream tip3p_dat("share/tip3p.xml");
+    AMBER ff(amber_dat);
+    ff.add_xml_file(tip3p_dat);
+
+    Spear::Simulation::initialize_plugins();
+
+    SECTION("Deca-Alanine") {
+        auto traj = chemfiles::Trajectory("data/input.pdb.gz");
+        auto frame = traj.read();
+
+        auto& first_res = *frame.topology().residues().begin();
+        const_cast<chemfiles::Residue&>(first_res).set("is_n_terminal", true);
+
+        auto& last_res = *(frame.topology().residues().begin() + 34);
+        const_cast<chemfiles::Residue&>(last_res).set("is_c_terminal", true);
+
+        auto pocket = Spear::Molecule(frame);
+        //pocket.add_bond(0,2);
+
+        Spear::Simulation sim;
+        ff.set_method(Spear::NonBondedForcefield::Ewald);
+        ff.set_cutoff(10.0);
+        sim.add_molecule(pocket, ff);
+        sim.set_periodic_vectors(frame.cell());
+        sim.add_non_bonded_force(ff);
+        sim.initialize_context("CUDA");
+
+        chemfiles::Trajectory otraj("villin.mmtf", 'w');
+        chemfiles::Frame start;
+        start.resize(pocket.size());
+        start.set_topology(pocket.topology());
+        update_chfl_frame(start, pocket.positions());
+
+        sim.minimize(1e-3, 10);
+        auto pos = sim.positions();
+        update_chfl_frame(start, pos);
+        otraj.write(start);
+
+        while (sim.time() <= 2) {
+            sim.dynamic_steps(10);
+            pos = sim.positions();
+            update_chfl_frame(start, pos);
+            otraj.write(start);
+        }
+    }
+}
