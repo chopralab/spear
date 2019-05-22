288aa4e52028c6f7834fb09edeed0b6bae00aa60
Jonathan Fine <finej@purdue.edu>
2019-05-21 21:14:05 -0400

    More functions for the SF interface

--- include/spear/ScoringFunction.hpp
+++ include/spear/ScoringFunction.hpp
@@ -21,7 +21,11 @@ public:
     /// Calculate the score of a given molecule
     virtual double score( const Grid& grid, const Molecule& mol1, const Molecule& mol2) const = 0;
 
+    /// Calculate the score of a molecule section
     virtual double score( const Grid& grid, const Molecule& mol, size_t residue_id) const = 0;
+
+    /// Calculate the score of two types with a given distance
+    virtual double score( size_t atomtype1, size_t atomtype2, double r) const = 0;
 };
 
 template<class ScoringFunction>
